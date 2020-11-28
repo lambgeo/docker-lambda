4c8d455d6e5f32deb4d4b082a7f8d44215e72ec5
vincentsarago <vincent.sarago@gmail.com>
2020-11-27 19:47:18 -0500

    remove elasticsearch for gdal3.2

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,9 +12,9 @@ docker build -f common/${IMAGE_VERSION}/Dockerfile -t ${IMAGE_VERSION}:build .
 docker build \
     --build-arg IMAGE_VERSION=${IMAGE_VERSION} \
     -f gdal${GDAL_VERSION}/Dockerfile \
-    -t lambgeo/lambda-${IMAGE_VERSION}:gdal${GDAL_VERSION} .
+    -t lambgeo/${IMAGE_VERSION}-gdal:${GDAL_VERSION} .
 
 docker build \
     --build-arg GDAL_VERSION=${GDAL_VERSION} \
     -f runtimes/${RUNTIME} \
-    -t lambgeo/lambda-${IMAGE_VERSION}:gdal${GDAL_VERSION}-${RUNTIME} .
+    -t lambgeo/${IMAGE_VERSION}-gdal:${GDAL_VERSION}-${RUNTIME} .
