a656fbf81e0a872a69950020290b309fe69a9ab6
vincentsarago <vincent.sarago@gmail.com>
2020-12-17 22:47:01 +0100

    try [skip ci]

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,14 +9,14 @@ LAMBDA_VERSION=$(echo $IMAGE_VERSION | sed -E 's/base(-)?//g')
 echo "Building image for Amazonlinux ${IMAGE_VERSION} | GDAL: ${GDAL_VERSION} | Runtime: ${RUNTIME}"
 
 # Base Image
-docker build -f common/${IMAGE_VERSION}/Dockerfile -t ${IMAGE_VERSION}:build .
+docker build -f dockerfiles/common/${IMAGE_VERSION}/Dockerfile -t ${IMAGE_VERSION}:build .
 
 docker build \
     --build-arg IMAGE_VERSION=${IMAGE_VERSION} \
-    -f gdal${GDAL_VERSION}/Dockerfile \
+    -f dockerfiles/gdal${GDAL_VERSION}/Dockerfile \
     -t lambgeo/lambda${LAMBDA_VERSION}-gdal:${GDAL_VERSION} .
 
 docker build \
     --build-arg GDAL_VERSION=${GDAL_VERSION} \
-    -f runtimes/${RUNTIME} \
+    -f dockerfiles/runtimes/${RUNTIME} \
     -t lambgeo/lambda-gdal:${GDAL_VERSION}-${RUNTIME} .
