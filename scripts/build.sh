2d2050ae7f813576923f5a630a8441b7904b7b7c
vincentsarago <vincent.sarago@gmail.com>
2020-12-22 17:01:47 +0100

    one commit to fix everything

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,8 @@ RUNTIME=$3
 
 LAMBDA_VERSION=$(echo $IMAGE_VERSION | sed -E 's/base(-)?//g')
 
+AZN_EXT=$(if [ "$IMAGE_VERSION" == "base-2" ]; then echo '-al2'; else echo ''; fi)
+
 echo "Building image for Amazonlinux ${IMAGE_VERSION} | GDAL: ${GDAL_VERSION} | Runtime: ${RUNTIME}"
 
 # Base Image
@@ -14,9 +16,9 @@ docker build -f dockerfiles/common/${IMAGE_VERSION}/Dockerfile -t ${IMAGE_VERSIO
 docker build \
     --build-arg IMAGE_VERSION=${IMAGE_VERSION} \
     -f dockerfiles/gdal${GDAL_VERSION}/Dockerfile \
-    -t lambgeo/lambda${LAMBDA_VERSION}-gdal:${GDAL_VERSION} .
+    -t lambgeo/lambda-gdal:${GDAL_VERSION}${AZN_EXT} .
 
-docker build \
-    --build-arg GDAL_VERSION=${GDAL_VERSION} \
-    -f dockerfiles/runtimes/${RUNTIME} \
-    -t lambgeo/lambda-gdal:${GDAL_VERSION}-${RUNTIME} .
+# docker build \
+#     --build-arg GDAL_VERSION=${GDAL_VERSION} \
+#     -f dockerfiles/runtimes/${RUNTIME} \
+#     -t lambgeo/lambda-gdal:${GDAL_VERSION}-${RUNTIME} .
