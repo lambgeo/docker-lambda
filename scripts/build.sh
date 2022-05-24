97c130834c7732467c3cc692a99954fe7f69b5f3
vincentsarago <vincent.sarago@gmail.com>
2022-05-24 13:24:37 +0200

    change cache

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,10 +7,12 @@ RUNTIME_VERSION=$3
 echo "Building image for AWS Lambda | GDAL: ${GDAL_VERSION} | Runtime: ${RUNTIME}:${RUNTIME_VERSION}"
 
 docker buildx build \
+    --platform=linux/amd64 \
     -f dockerfiles/Dockerfile.gdal${GDAL_VERSION} \
     -t ghcr.io/lambgeo/lambda-gdal:${GDAL_VERSION} .
 
 docker buildx build \
+    --platform=linux/amd64 \
     --build-arg GDAL_VERSION=${GDAL_VERSION} \
     --build-arg RUNTIME_VERSION=${RUNTIME_VERSION} \
     -f dockerfiles/runtimes/${RUNTIME} \
