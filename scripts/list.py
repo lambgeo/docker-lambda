4cb32cc8d0ef38da3a8fac8af92d029eb5bcee7b
Rustem <rustem@tintash.com>
2022-08-11 16:22:24 +0200

    updating SQLite libs, GDAL libs and GEOS libs

--- scripts/list.py
+++ scripts/list.py
@@ -3,22 +3,22 @@ import json
 from boto3.session import Session as boto3_session
 
 AWS_REGIONS = [
-    "ap-northeast-1",
-    "ap-northeast-2",
-    "ap-south-1",
-    "ap-southeast-1",
-    "ap-southeast-2",
-    "ca-central-1",
-    "eu-central-1",
-    "eu-north-1",
-    "eu-west-1",
-    "eu-west-2",
-    "eu-west-3",
-    "sa-east-1",
+    # "ap-northeast-1",
+    # "ap-northeast-2",
+    # "ap-south-1",
+    # "ap-southeast-1",
+    # "ap-southeast-2",
+    # "ca-central-1",
+    # "eu-central-1",
+    # "eu-north-1",
+    # "eu-west-1",
+    # "eu-west-2",
+    # "eu-west-3",
+    # "sa-east-1",
     "us-east-1",
-    "us-east-2",
-    "us-west-1",
-    "us-west-2",
+    # "us-east-2",
+    # "us-west-1",
+    # "us-west-2",
 ]
 layers = [
     # "gdal24",  # archive
